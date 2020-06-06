whateverany/matrix-plugin

commit d287654a23c7e797c5b36ca61ac753d660b0a2c0

Use the scheduler provided by the API for Sync

polo-bukkit/src/main/java/dev/dhdf/polo/bukkit/Main.java
@@ -58,13 +58,8 @@ public class Main extends JavaPlugin implements PoloPlugin {
 
         if (vibeCheck) {
             logger.finer("Started bridge");
-            Timer timer = new Timer();
-            TimerTask timerTask = new Sync(webClient);
-            timer.scheduleAtFixedRate(
-                    timerTask,
-                    0,
-                    5000
-            );
+            Sync sync = new Sync(webClient);
+            getServer().getScheduler().runTaskTimerAsynchronously(this, sync, 0, 5000);
         } else {
             logger.severe("Couldn't properly connect to marco is the address and port set properly?");
         }

polo-common/src/main/java/dev/dhdf/polo/util/Sync.java
@@ -9,7 +9,7 @@ import java.util.TimerTask;
  * This class gets the chat from Marco in a set interval
  * (see Main class)
  */
-public class Sync extends TimerTask {
+public class Sync implements Runnable {
     private final WebClient client;
 
     public Sync(WebClient client) {

commit c078b89ee58535ee65122d73dc199eb7e7ea1b8c

Fix timer period to 5sec instead of >4mins
Commit d287654a23c7 ("Use the scheduler provided by the API for Sync")
switched to using runTaskTimerAsynchronously(), but it appears that the
period is measured in ticks, not milliseconds, so the value of
5000 is actually over 4 minutes. This causes long delays for MX->MC
messages to get through.

Fix it to 5*20 ticks, 5 seconds. I'm assuming ticks are always at a rate
of about 20Hz, since I can't find any API to get the tick rate and from
what I've read its not configurable.

The relevant documentation which seems to still apply is here:
https://jd.bukkit.org/org/bukkit/scheduler/BukkitScheduler.html#runTaskTimerAsynchronously(org.bukkit.plugin.Plugin,%20java.lang.Runnable,%20long,%20long)

Fixes: d287654a23c7 ("Use the scheduler provided by the API for Sync")

polo-bukkit/src/main/java/dev/dhdf/polo/bukkit/Main.java
@@ -59,7 +59,7 @@ public class Main extends JavaPlugin implements PoloPlugin {
         if (vibeCheck) {
             logger.finer("Started bridge");
             Sync sync = new Sync(webClient);
-            getServer().getScheduler().runTaskTimerAsynchronously(this, sync, 0, 5000);
+            getServer().getScheduler().runTaskTimerAsynchronously(this, sync, 0, 5*20);
         } else {
             logger.severe("Couldn't properly connect to marco is the address and port set properly?");
         }