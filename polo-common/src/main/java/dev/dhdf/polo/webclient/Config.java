0f872e8e164e5dbfa3dc1bb82b1583f94b3bc425
James Hogan <james@albanarts.com>
2020-06-23 17:37:31 +0100

    Optionally handle kicks, bans & unbans from Matrix
    If matrix-appservice-minecraft reports kicks, bans or unbans, optionally
    relay those on to the Minecraft server if the corresponding
    relay-mx-kicks or relay-mx-bans are set in the config.yml.
    
    Handling is implemented only for Bukkit at the moment, where we do
    currently broadcast the events to players like /kick, /ban & /unban do,
    but not with localized text or necessarily matching default reasons
    (e.g. EssentialsX reimplements them).

--- polo-common/src/main/java/dev/dhdf/polo/webclient/Config.java
+++ polo-common/src/main/java/dev/dhdf/polo/webclient/Config.java
@@ -9,12 +9,18 @@ public class Config {
     public final int port;
     public final String token;
     public final boolean relay_mc_membership;
+    public final boolean relay_mx_kicks;
+    public final boolean relay_mx_bans;
 
     public Config(String address, int port, String token,
-                  boolean relay_mc_membership) {
+                  boolean relay_mc_membership,
+                  boolean relay_mx_kicks,
+                  boolean relay_mx_bans) {
         this.address = address;
         this.port = port;
         this.token = token;
         this.relay_mc_membership = relay_mc_membership;
+        this.relay_mx_kicks = relay_mx_kicks;
+        this.relay_mx_bans = relay_mx_bans;
     }
 }
